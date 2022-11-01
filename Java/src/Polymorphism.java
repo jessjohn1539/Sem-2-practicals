5a357b7d825de6a08acb560763da30bc54d8cb10
Jess John <jessjohn1539@gmail.com>
2022-11-02 00:08:05 +0530

    modified:   Java/src/Polymorphism.java

--- Java/src/Polymorphism.java
+++ Java/src/Polymorphism.java
@@ -16,7 +16,7 @@ class Student {
 }
 
 public class Polymorphism {
-    public static void main() {
+    public static void main(String args[]) {
         Student s1 = new Student();
         s1.name = "Jess";
         s1.age = 19;
