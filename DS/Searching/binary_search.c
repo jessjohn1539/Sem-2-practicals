de24e6d00148b011b2e3146c450bb4aa99a0d9ca
Jess John <jessjohn1539@gmail.com>
2022-11-03 19:28:16 +0530

    new file:   DS/Searching/binary_search.c

---
+++ DS/Searching/binary_search.c
@@ -0,0 +1,25 @@
+/*
+step 1: input an array from user.
+step 2: sort it in ascending order.
+step 3: mark starting point and ending point as i and j.
+step 4: find centre using c=(i+j)/2
+        if(key==a[c]){
+        printf("Value found");
+        break;
+        }
+        if(key>a[c])
+        {
+        centre ke left mei search karna hai by
+        changing the value of j to c-1.
+        }
+        if(key>a[c])
+        {
+         centre ke right mei search karna hai by
+         changing the value of i to c-1.
+        }
+*/
+#include <stdio.h>
+
+void main()
+{
+}
